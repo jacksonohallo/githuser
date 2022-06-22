c8fca3955ec970d2c46e91250b0e1fa4bd96532c
jacksonohallo <jacksonohallo@gmail.com>
2022-06-22 10:44:38 +0300

    add fetch

--- src/main.js
+++ src/main.js
@@ -3,3 +3,10 @@ const main = document.getElementsByClassName("main");
 const form = document.getElementsByClassName("form");
 const search = document.getElementsByClassName("search");
 getUser("jacksonohallo");
+
+async function getUser(username) {
+    const res = await fetch(API_URL + username);
+    const resData = await Response.json();
+    createUserCard(resData);
+    getRespos(username);
+}
