ddd5af575eb1d748f03211333df03ab7dd829594
jacksonohallo <jacksonohallo@gmail.com>
2022-06-22 10:35:47 +0300

    selecting html elements into js

--- src/main.js
+++ src/main.js
@@ -1,2 +1,5 @@
 const API_URL = "https://api.github.com/users/";
-
+const main = document.getElementsByClassName("main");
+const form = document.getElementsByClassName("form");
+const search = document.getElementsByClassName("search");
+getUser("jacksonohallo");
